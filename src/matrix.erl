1f71004f61e531f9a3d47ea6e7ec7f32936f20a8
Tom Burdick <tburdick@wrightwoodtech.com>
2011-11-09 12:24:26 -0600

    mulling over the api first and foremost more, thinking more that a matrix can be a tagged tuple and that the actual numerical values can be machine specifically encoded in an erlang binary

--- src/matrix.erl
+++ src/matrix.erl
@@ -1,11 +1,14 @@
 %% @author Tom Burdick <thomas.burdick@gmail.com>
 %% @copyright 2011 Tom Burdick
-%% @doc Matrix Creation and Manipulation Functions.
+%% @doc Numerical erlang.
 
 -module(matrix).
 
+-type type() :: int8 | int16 | int32 | int64 | real32
+    | real64 | complex32 | complex64.
 -type dimensions() :: pos_integer() | tuple(pos_integer()).
--type matrix() :: binary().
+-type index() :: pos_integer() | tuple(pos_integer()).
+-type matrix() :: {matrix, type(), dimensions(), binary()}.
 -type tuple_matrix() :: tuple(number())
     | tuple(tuple(number()))
     | tuple(tuple(tuple(number()))).
@@ -13,16 +16,30 @@
     | list(list(number()))
     | list(list(list(number()))).
 
--export_type([dimensions/0, matrix/0, tuple_matrix/0, list_matrix/0]).
+-export_type([type/0, dimensions/0, index/0, matrix/0, tuple_matrix/0, list_matrix/0]).
 
 -define(NOT_LOADED, not_loaded(?LINE)).
 
--export([identity/1, ones/1, zeros/1, multiply/2,
-    transpose/1]).
+-export([new/1, identity/1, ones/1, zeros/1,
+        col/2, row/2, cell/2, add/2, multiply/2,
+        transpose/1, invert/1]).
 
 -on_load(init/0).
 
+
 %% api
+
+%% @doc Create a matrix from a list of lists or a tuple of tuples.
+-spec new(list_matrix() | tuple_matrix()) -> matrix().
+new(M0) ->
+    ?NOT_LOADED.
+
+%% @doc Create a specific kind of matrix from a list of lists or a tuple
+%% of tuples.
+%% @end
+-spec new(type(), list_matrix() | tuple_matrix()) -> matrix().
+new(T, M0) ->
+    ?NOT_LOADED.
 
 %% @doc Create an identity matrix.
 -spec identity(dimensions()) -> matrix().
@@ -39,14 +56,39 @@ ones(Dimensions) ->
 zeros(Dimensions) ->
     ?NOT_LOADED.
 
-%% @doc Multiply two matrices.
--spec multiply(matrix(), matrix()) -> matrix().
+%% @doc Column of a matrix.
+-spec col(matrix(), pos_integer()) -> matrix().
+col(M0, I) ->
+    ?NOT_LOADED.
+
+%% @doc Row of a matrix.
+-spec row(matrix(), pos_integer()) -> matrix().
+row(M0, I) ->
+    ?NOT_LOADED.
+
+%% @doc Cell value.
+-spec cell(matrix(), index()) -> number().
+cell(M0, I) ->
+    ?NOT_LOADED.
+
+%% @doc Add. Performs a scalar or matrix addition.
+-spec add(matrix(), number() | matrix()) -> matrix().
+add(M0, M1) ->
+    ?NOT_LOADED.
+
+%% @doc Multiply. Performs a scalar or matrix multiply.
+-spec multiply(matrix(), number() | matrix()) -> matrix().
 multiply(M0, M1) ->
     ?NOT_LOADED.
 
 %% @doc Transpose a matrix.
 -spec transpose(matrix()) -> matrix().
 transpose(M0) ->
+    ?NOT_LOADED.
+
+%% @doc Invert a matrix.
+-spec invert(matrix()) -> matrix() | {error, not_invertable}.
+invert(M0) ->
     ?NOT_LOADED.
 
 %% @doc Convert a matrix to a tuple of tuples.
